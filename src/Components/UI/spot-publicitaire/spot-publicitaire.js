9b92a5535555cf10d24abe3c36f2fb6fe2c63643
ElrouDeveloper <rihab.berrichiie@gmail.com>
2021-08-05 23:47:00 +0200

    Added Spot Publicitaire Component

---
+++ src/Components/UI/spot-publicitaire/spot-publicitaire.js
@@ -0,0 +1,24 @@
+
+import React from "react";
+import "./spot-publicitaire.css"
+
+
+
+const SpotPublicitaire = () =>
+{
+
+    return (
+        <div className="spot"> <h1 >Luxurious Rooms
+
+        </h1>
+            <div> <p className="spot-price"> DeLuxe Rooms Starting at $299</p>
+
+            </div>
+            <button><a href="/rooms">Our Rooms</a></button>
+        </div>
+
+
+    );
+
+}
+export default SpotPublicitaire;
