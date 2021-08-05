92f712288a2c5acbdf3e27350f3ab2824d91fd45
ElrouDeveloper <rihab.berrichiie@gmail.com>
2021-08-05 23:44:56 +0200

    Added Home Component

---
+++ src/Components/Screens/Home/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import SpotPublicitaire from "../../UI/spot-publicitaire/spot-publicitaire";
+import "./Home.css"
+
+const Home = () =>
+{
+
+    return (
+        <div>
+            <SpotPublicitaire />
+        </div>
+    );
+
+}
+export default Home;
