212abd052bb59aeb6e694b30d9a25d2a913e8cae
ElrouDeveloper <rihab.berrichiie@gmail.com>
2021-08-11 02:29:30 +0200

    clean up

--- src/Components/UI/NavBar/NavBar.js
+++ src/Components/UI/NavBar/NavBar.js
@@ -1,17 +1,25 @@
+import "./NavBar.css";
 
-import "./NavBar.css"
-
-const NavBar = () =>
-{
-    return (
-        <div className="NavBar">
-            <ul className="Ul-navBar">
-                <li><a className="active" href="/home">Home</a></li>
-                <li><a href="/rooms">Rooms</a></li>
-                <li><a href="/contact">Contact</a></li>
-                <li><a href="/about">About</a></li>
-            </ul>
-
-        </div>)
-}
+const NavBar = () => {
+  return (
+    <div className="NavBar">
+      <ul className="Ul-navBar">
+        <li>
+          <a className="active" href="/home">
+            Home
+          </a>
+        </li>
+        <li>
+          <a href="/rooms">Rooms</a>
+        </li>
+        <li>
+          <a href="/contact">Contact</a>
+        </li>
+        <li>
+          <a href="/about">About</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
 export default NavBar;
